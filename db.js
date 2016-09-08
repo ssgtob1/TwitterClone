ffaf89ec963d58f074998bc529a35493ba7fdb31
Bill O'Brien <airman.obrien@gmail.com>
2016-09-08 10:08:12 -0400

    changed db.js to add password select

--- db.js
+++ db.js
@@ -12,33 +12,56 @@ function insertUser(db, user) {
                     stmt.run(user.userid, user.password, user.firstname, user.lastname, user.email),
                         function(err) {
                             if (err) {
+                                stmt.finalize();
                                 db.close();
                                 reject(err);
                                 return;
                             }
+                            stmt.finalize();
                             db.close();
                             resolve();
-                        });
-            });
+                        })
+            };
+        })
 
 
+
+}
+
+function inserTweet(db, tweet) {
+    return new Promise(
+        (resolve, reject) => {
+            db.serialize(function() {
+                    var stmt = db.prepare("INSERT INTO tweets  VALUES(?,?,?)");
+                    stmt.run(tweet.userid, tweet.tweetcontent, tweet.tweettsfunction(err) {
+                        if (err) {
+                            stmt.finalize();
+                            db.close();
+                            reject(err);
+                            return;
+                        }
+                        stmt.finalize();
+                        db.close();
+                        resolve();
+                    })
+                };
+            })
 
     }
 
-    function inserTweet(db, tweet) {
+    function getPassword(db, user) {
         return new Promise(
-                (resolve, reject) => {
-                    db.serialize(function() {
-                        var stmt = db.prepare("INSERT INTO tweets  VALUES(?,?,?)");
-                        stmt.run(tweet.userid, tweet.tweetcontent, tweet.tweettsfunction(err) {
-                            if (err) {
-                                db.close();
-                                reject(err);
-                                return;
-                            }
+            db.serialize(function() {
+                (reslove, reject) => {
+                    db.each("SELECT password FROM user WHERE userid = " + user.userid, function(err, row) {
+                        if (err) {
                             db.close();
-                            resolve();
-                        });
+                            reject(err);
+                            return;
+                        }
+                        resolve(row.password);
                     });
-
                 }
+            });
+        )
+    }
